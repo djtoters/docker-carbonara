fe2707d331609dd0865adb7c872c970a855336ef
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-29 14:36:17 +0200

    workflow test

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,10 +4,10 @@ from odoo.exceptions import UserError
 
 
 class EstateProperty(models.Model):
-    _name = 'estate.property_estate'
-    _description = 'Property of estate module XDc' 
-    _rec_name = 'title'
-    _order = 'sequence, id desc'
+    _name = "estate.property_estate"
+    _description = "Property of estate module XdcchvsDc"
+    _rec_name = "title"
+    _order = "sequence, id desc"
 
     @api.depends("living_area", "garden_area")
     def _compute_total(self):
@@ -17,7 +17,11 @@ class EstateProperty(models.Model):
     @api.depends("offer_ids.price")
     def _best_offer(self):
         for record in self:
-            record.best_offer = max(record.offer_ids.mapped('price')) if record.offer_ids else 0
+            record.best_offer = (
+                max(record.offer_ids.mapped("price"))
+                if record.offer_ids
+                else 0
+            )
 
     @api.onchange("garden")
     def _garden_on_change(self):
@@ -28,20 +32,28 @@ class EstateProperty(models.Model):
             self.garden_orientation = "South"
             self.garden_area = None
 
-    sequence = fields.Integer('Sequence', default=1, help="Used to order property. Lower is better.")
-    title = fields.Char(string='Name of the property')
-    description = fields.Char(string='Description', required=True)
-    postcode = fields.Char(string='Postcode', required=True)
-    date_availability = fields.Date(string='Available from', required=True, copy=False,
-                                    default=lambda self: fields.Datetime.today() + timedelta(days=90))
-    expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    sequence = fields.Integer(
+        "Sequence", default=1, help="Used to order property. Lower is better."
+    )
+    title = fields.Char(string="Name of the property")
+    description = fields.Char(string="Description", required=True)
+    postcode = fields.Char(string="Postcode", required=True)
+    date_availability = fields.Date(
+        string="Available from",
+        required=True,
+        copy=False,
+        default=lambda self: fields.Datetime.today() + timedelta(days=90),
+    )
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(
+        string="Selling Price", readonly=True, copy=False
+    )
     best_offer = fields.Float(string="Best offer", compute="_best_offer")
-    bedroom = fields.Integer(string='Bedroom', default='3')
-    facade = fields.Integer(string='Facade')
-    garage = fields.Boolean(string='Garage')
+    bedroom = fields.Integer(string="Bedroom", default="3")
+    facade = fields.Integer(string="Facade")
+    garage = fields.Boolean(string="Garage")
     roof = fields.Boolean(string="Roof", default=True)
-    garden = fields.Boolean(string='Garden')
+    garden = fields.Boolean(string="Garden")
     garden_orientation = fields.Selection(
         [
             ("North", "North"),
@@ -49,14 +61,14 @@ class EstateProperty(models.Model):
             ("West", "West"),
             ("East", "East"),
         ],
-        string='Garden orientation',
-        default='North',
-        required=True
+        string="Garden orientation",
+        default="North",
+        required=True,
     )
-    garden_area = fields.Float(string='Garden Area')
-    living_area = fields.Float(string='Living Area')
-    total_area = fields.Float(string='Total Area', compute="_compute_total")
-    active = fields.Boolean(string='Active', default=True)
+    garden_area = fields.Float(string="Garden Area")
+    living_area = fields.Float(string="Living Area")
+    total_area = fields.Float(string="Total Area", compute="_compute_total")
+    active = fields.Boolean(string="Active", default=True)
     status = fields.Selection(
         [
             ("New", "New"),
@@ -65,21 +77,38 @@ class EstateProperty(models.Model):
             ("Sold", "Sold"),
             ("Canceled", "Canceled"),
         ],
-        string='Satus',
+        string="Satus",
         required=True,
-        default='New',
-        copy=False
+        default="New",
+        copy=False,
     )
-    property_type_id = fields.Many2one(comodel_name="estate.property_estate_type", string="Property type")
-    buyer_id = fields.Many2one(comodel_name="res.partner", string="Buyer", copy=False)
-    salesperson_id = fields.Many2one(comodel_name="res.users", string="Salesperson", default=lambda self: self.env.user)
-    tags_ids = fields.Many2many(comodel_name="estate.property_estate_tag", string="Tags")
-    offer_ids = fields.One2many(comodel_name="estate.property_estate_offer", inverse_name="property_id",
-                                string="Offers")
+    property_type_id = fields.Many2one(
+        comodel_name="estate.property_estate_type", string="Property type"
+    )
+    buyer_id = fields.Many2one(
+        comodel_name="res.partner", string="Buyer", copy=False
+    )
+    salesperson_id = fields.Many2one(
+        comodel_name="res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user,
+    )
+    tags_ids = fields.Many2many(
+        comodel_name="estate.property_estate_tag", string="Tags"
+    )
+    offer_ids = fields.One2many(
+        comodel_name="estate.property_estate_offer",
+        inverse_name="property_id",
+        string="Offers",
+    )
     # model_id = fields.Many2one(comodel_name="estate.property_estate_type")
 
     _sql_constraints = [
-        ('positive_price', 'CHECK(expected_price > 0)', 'The price must be strictly positive')
+        (
+            "positive_price",
+            "CHECK(expected_price > 0)",
+            "The price must be strictly positive",
+        )
     ]
 
     def action_cancel(self):
@@ -92,13 +121,13 @@ class EstateProperty(models.Model):
             if record.status == "Canceled":
                 raise UserError("Cancelled properties cannot be sold!")
             else:
-                record.status = 'Sold'
+                record.status = "Sold"
             return True
 
     @api.ondelete(at_uninstall=False)
     def _prevent_deletion(self):
         for record in self:
             if record.status not in ["New", "Canceled"]:
-                raise UserError("Only New ans Cancelled properties can be deleted!")
-
-
+                raise UserError(
+                    "Only New ans Cancelled properties can be deleted!"
+                )
