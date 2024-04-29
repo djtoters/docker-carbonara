b052b25c6b240d554461528a6e4973e106858991
djtoters <dubois.florian@me.com>
2024-04-29 11:13:42 +0200

    first commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Estate",
+    "version": "1.0",
+    "category": "Sales/estate",
+    "sequence": 1000,
+    "summary": "Real estate module",
+    "description": "",
+    "website": "https://www.odoo.com/documentation/15.0/fr/developer/tutorials/getting_started/03_newapp.html",
+    "depends": [
+        "base",
+    ],
+    "data": [
+   
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
