3a1b1fbbf7fa7e86c11958d320654e02dc1b1320
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-30 14:21:50 +0200

    pull request

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = "estate.property_estate"
-    _description = "Property of estate module XvqdggshsDddfc"
+    _description = "Property of estate module Xvqdgddfc"
     _rec_name = "title"
     _order = "sequence, id desc"
 
