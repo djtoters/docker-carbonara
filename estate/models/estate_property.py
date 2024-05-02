b0ca0d6825ef9dbd21d0d25d1f79952ff9a9207d
Maxime007XD <mdangelo2002pro@gmail.com>
2024-05-02 11:39:38 +0200

    test sans flag

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = "estate.property_estate"
-    _description = "Property of estate module XvggtfgagfgesJJESUdSLEGOATsteregddXDDdfdtestfc"
+    _description = "Property of estate module XvggtfgfgesJJESUdSLEGOATsteregddXDDdfdtestfc"
     _rec_name = "title"
     _order = "sequence, id desc"
 
