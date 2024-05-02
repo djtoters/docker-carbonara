41f3f24917f883deb68ad32dd733299fdc204bcd
Maxime007XD <mdangelo2002pro@gmail.com>
2024-05-02 11:42:03 +0200

    test avec flag PR mais ecriture

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = "estate.property_estate"
-    _description = "Property of estate module XvggtfgfgesJJESUdSLEGOATsteregddXDDdfdtestfc"
+    _description = "Property of estate module XvggtfgesJJESUdSLEGOATsteregddXDDdfdtestfc"
     _rec_name = "title"
     _order = "sequence, id desc"
 
