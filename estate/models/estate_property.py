88874a9ce7924de915744f1d8bd3bac4284087fa
Maxime007XD <mdangelo2002pro@gmail.com>
2024-05-02 08:59:06 +0200

    Champion du monde

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = "estate.property_estate"
-    _description = "Property of estate module XvggqdtgoatesteregddXDDdfdtestfc"
+    _description = "Property of estate module XvggqdtgoateJJESUISLEGOATsteregddXDDdfdtestfc"
     _rec_name = "title"
     _order = "sequence, id desc"
 
