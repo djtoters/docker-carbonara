5c73eecee71be0f6ef940e895cc2764cabf14b1e
Maxime007XD <mdangelo2002pro@gmail.com>
2024-05-02 09:29:15 +0200

    test combinaison des wkf

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = "estate.property_estate"
-    _description = "Property of estate module XvggqdtfdggoatersrddJJESUddISLEGOATsteregddXDDdfdtestfc"
+    _description = "Property of estate module XvggqdtfdgdfgdggoatersrddJJESUddISLEGOATsteregddXDDdfdtestfc"
     _rec_name = "title"
     _order = "sequence, id desc"
 
